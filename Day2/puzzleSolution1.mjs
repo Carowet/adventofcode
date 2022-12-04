eab3d29eba75af15d26ba9b90e29510113eadc79
Caroline Wetzel <c.wetzel@tbs-education.org>
2022-12-04 15:43:51 +0100

    Day 2

--- Day2/puzzleSolution1.mjs
+++ Day2/puzzleSolution1.mjs
@@ -20,6 +20,24 @@ const mapInput = {
   Z: moves.scissors,
 };
 
+const solution = {
+  A: {
+    X: moves.scissors,
+    Y: moves.rock,
+    Z: moves.paper,
+  },
+  B: {
+    X: moves.rock,
+    Y: moves.paper,
+    Z: moves.scissors,
+  },
+  C: {
+    X: moves.paper,
+    Y: moves.scissors,
+    Z: moves.rock,
+  },
+};
+
 function score(opponentMove, myMove) {
   if (opponentMove === myMove) {
     return myMove + 3;
@@ -43,4 +61,16 @@ function part1() {
   console.log(outcomes.reduce((a, b) => a + b, 0));
 }
 
+function part2() {
+  const outcomes = lines.map((line) => {
+    const opponentMove = mapInput[line[0]];
+
+    const ourMove = solution[line[0]][line[1]];
+
+    return score(opponentMove, ourMove);
+  });
+  console.log(outcomes.reduce((a, b) => a + b, 0));
+}
+
 part1();
+part2();
