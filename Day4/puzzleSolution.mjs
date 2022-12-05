5cc3ce24872f943a2bee212c5ed40666f21e85a1
Caroline Wetzel <c.wetzel@tbs-education.org>
2022-12-05 22:30:51 +0100

    Day 4 Part 1

---
+++ Day4/puzzleSolution.mjs
@@ -0,0 +1,25 @@
+import { readFileSync } from "node:fs";
+
+const lines = readFileSync("puzzleInput.txt", { encoding: "utf-8" })
+  .trim() // Remove starting/ending whitespace
+  .split("\n") // Split on newline
+
+function part1() {
+  const result = lines.map((line) => {
+    const [interval1, interval2] = line
+      .split(",")
+      .map((interval) => interval.split("-").map(Number))
+      .sort((a, b) => {
+        const oneSide = a[1] - a[0];
+        const twoSide = b[1] - b[0];
+        return twoSide - oneSide;
+      });
+
+    const oneFullContainsTwo = interval1[0] <= interval2[0] && interval1[1] >= interval2[1];
+
+    return oneFullContainsTwo ? 1 : 0
+  })
+  console.log(result.reduce((a, b) => a + b, 0))
+}
+
+part1();
