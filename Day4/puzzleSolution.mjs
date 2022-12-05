e8b1104c80134973c91f7d8443dcf44caf7249ef
Caroline Wetzel <c.wetzel@tbs-education.org>
2022-12-05 22:55:06 +0100

    Day 4 Part 2

--- Day4/puzzleSolution.mjs
+++ Day4/puzzleSolution.mjs
@@ -22,4 +22,23 @@ function part1() {
   console.log(result.reduce((a, b) => a + b, 0))
 }
 
+function part2() {
+  const result = lines.map((line) => {
+    const [interval1, interval2] = line
+      .split(",")
+      .map((interval) => interval.split("-").map(Number))
+      .sort((a, b) => {
+        const oneSide = a[1] - a[0];
+        const twoSide = b[1] - b[0];
+        return twoSide - oneSide;
+      });
+
+    const overlap = interval1[0] <= interval2[1] && interval1[1] >= interval2[0];
+
+    return overlap ? 1 : 0
+  })
+  console.log(result.reduce((a, b) => a + b, 0))
+}
+
 part1();
+part2();
