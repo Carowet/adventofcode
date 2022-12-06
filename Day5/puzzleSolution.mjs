fa4b4930384227609b600cf711d5f3fb72464b9f
Caroline Wetzel <c.wetzel@tbs-education.org>
2022-12-06 22:31:48 +0100

    Day 5 part 2

--- Day5/puzzleSolution.mjs
+++ Day5/puzzleSolution.mjs
@@ -53,4 +53,21 @@ function part1() {
   );
 }
 
+function part2() {
+  const localStacks = JSON.parse(JSON.stringify(stacks));
+  for (const move of moves) {
+    const crates = localStacks[move.from].splice(-move.count, move.count);
+    localStacks[move.to] = localStacks[move.to].concat(crates);
+  }
+  console.log(
+    indexes
+      .map((value) => {
+        const stack = localStacks[value];
+        return stack[stack.length - 1];
+      })
+      .join("")
+  );
+}
+
 part1();
+part2();
